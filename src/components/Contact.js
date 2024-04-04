22a02c752df6b4fee008eabe396687b16e11f557
aarjookhand <03aarjoo@gmail.com>
2024-04-04 16:10:18 +0200

    setup setup

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+      <div className="contact-info"> ph num</div>
+      <div className="email"> aarjookhand@outlook.com,    
+      03aarjoo@gmail.com </div>
+      
+      
+    </div>
+  )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/home.css'
+
+export default function Home() {
+  return (
+    <div>
+        <div className="abt-me">About me</div>
+        <div className="abt-me-text">Salut ! I'm  Aarjoo, a second-year Computer Science student, driven by the endless possibilities and my passion for tech in the heart of Paris, currently pursuing my Bachelor's degree here at EPITA. Let's connect, collaborate, and create something amazing together!</div>
+      
+    </div>
+  )
+}

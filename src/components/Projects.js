dc8554e600ae74a7090c2caf471a685daed032cc
aarjookhand <03aarjoo@gmail.com>
2024-04-07 23:52:38 +0200

    outline

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,11 +4,14 @@ import '../styles/project.css'
 export default function Projects() {
   return (
     <div>
+      <h1>My Projects</h1>
       <div className="project">
         <div className="project-title">Twitter-Clone </div>
-        <div className="project-description"> created a twitter clone </div>
+        <div className="project-description"> The Express-React Twitter Clone is a full-stack web application that replicates the core functionality of Twitter/ X. Built with Express.js for the backend and React.js for the frontend, the project demonstrates implementation of key features such as user authentication, posting tweets, following other users, liking and retweeting posts, and real-time updates. </div>
         <div className="project-ss"></div>
-        <div className="project-link"></div>
+        <div className="project-link">
+        <a href="https://github.com/aarjookhand">Git repo</a>
+        </div>
       </div>
 
       <div className="project">
